7d2498e84789f8b7cf25a9bfa63fd2cafe1f02b1
tidzejkej <tomolol111@gmail.com>
2024-08-16 16:26:40 +0200

    dd proxy test #29

--- app/Services/Proxies/BaselinkerIntegrationProxyService.php
+++ app/Services/Proxies/BaselinkerIntegrationProxyService.php
@@ -27,54 +27,20 @@ class BaselinkerIntegrationProxyService
     {
         if(isset($parameters[0])) $parameters = $parameters[0];
         return $this->post($method, $parameters);
-
-       /* foreach ($this->availableInterfaces as $interface) {
-            try {
-                $interfaceInstance = $client->$interface();
-                if (method_exists($interfaceInstance, $method)) {
-                    $params = $parameters[0] ?? $parameters;
-                    error_log("Interface instance: " . get_class($interfaceInstance));
-
-                    $reflection = new \ReflectionMethod($interfaceInstance, $method);
-                    $args = [];
-
-                    foreach ($reflection->getParameters() as $param) {
-                        $paramName = $param->getName();
-
-                        if (isset($params[$paramName])) {
-                            $args[] = $params[$paramName];
-                        } elseif ($param->isDefaultValueAvailable()) {
-                            $args[] = $param->getDefaultValue();
-                        } else {
-                            throw new \InvalidArgumentException("Missing parameter: {$paramName}");
-                        }
-                    }
-                    $response = call_user_func_array([$interfaceInstance, $method], $args);
-
-                    return json_decode($response->contents(), true);
-                }
-            } catch (\Exception $e) {
-                continue;
-            }
-        }
-
-        throw new \BadMethodCallException("Method {$method} does not exist on any available interfaces.");*/
     }
 
-
-
-    private function makeClient(): ?Baselinker
-    {
-        try {
-            $data = $this->repository->getIntegrationData();
-            return new Baselinker(['token' => $data[BaselinkerIntegrationEnum::Token->value]['value']] ?? []);
-        } catch (\Exception){
-            return null;
-        }
-    }
 
     protected function post(string $function, array $parameters = []): ResponseInterface
     {
+        dd($this->client()->post('connector.php', [
+            RequestOptions::HEADERS => [
+                'X-BLToken' => $this->repository->getIntegrationData()[BaselinkerIntegrationEnum::Token->value]['value'],
+            ],
+            RequestOptions::FORM_PARAMS => [
+                'method' => $function,
+                'parameters' => json_encode($parameters),
+            ],
+        ]));
         return $this->client()->post('connector.php', [
             RequestOptions::HEADERS => [
                 'X-BLToken' => $this->repository->getIntegrationData()[BaselinkerIntegrationEnum::Token->value]['value'],
