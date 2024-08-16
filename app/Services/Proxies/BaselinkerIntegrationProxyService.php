b8b4340609b567eb69531b120bf4c6ea18d2b58d
tidzejkej <tomolol111@gmail.com>
2024-08-16 13:03:09 +0200

    dd proxy test #4

--- app/Services/Proxies/BaselinkerIntegrationProxyService.php
+++ app/Services/Proxies/BaselinkerIntegrationProxyService.php
@@ -20,13 +20,31 @@ class BaselinkerIntegrationProxyService
     public function __call($method, $parameters)
     {
         $client = $this->makeClient();
+
         foreach ($this->availableInterfaces as $interface) {
             try {
                 $interfaceInstance = $client->$interface();
 
                 if (method_exists($interfaceInstance, $method)) {
-                   // dd($interfaceInstance, $method, $parameters, request()->all());
-                    return json_decode(call_user_func([$interfaceInstance, $method], $parameters)?->contents(), true);
+                    $params = $parameters[0] ?? $parameters;
+
+                    $reflection = new \ReflectionMethod($interfaceInstance, $method);
+                    $args = [];
+
+                    foreach ($reflection->getParameters() as $param) {
+                        $paramName = $param->getName();
+
+                        if (isset($params[$paramName])) {
+                            $args[] = $params[$paramName];
+                        } elseif ($param->isDefaultValueAvailable()) {
+                            $args[] = $param->getDefaultValue();
+                        } else {
+                            throw new \InvalidArgumentException("Missing parameter: {$paramName}");
+                        }
+                    }
+                    $response = call_user_func_array([$interfaceInstance, $method], $args);
+
+                    return json_decode($response->contents(), true);
                 }
             } catch (\Exception $e) {
                 continue;
@@ -35,6 +53,7 @@ class BaselinkerIntegrationProxyService
 
         throw new \BadMethodCallException("Method {$method} does not exist on any available interfaces.");
     }
+
 
 
     private function makeClient(): ?Baselinker
