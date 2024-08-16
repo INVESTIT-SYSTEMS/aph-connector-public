66f03c486a5ff8d69b366000f7ef298d123737e8
tidzejkej <tomolol111@gmail.com>
2024-08-16 15:58:45 +0200

    dd proxy test #25

--- app/Services/Proxies/BaselinkerIntegrationProxyService.php
+++ app/Services/Proxies/BaselinkerIntegrationProxyService.php
@@ -76,6 +76,16 @@ class BaselinkerIntegrationProxyService
 
     protected function post(string $function, array $parameters = []): ResponseInterface
     {
+        $response = $this->client()->post('connector.php', [
+            RequestOptions::HEADERS => [
+                'X-BLToken' => $this->repository->getIntegrationData()[BaselinkerIntegrationEnum::Token->value]['value'],
+            ],
+            RequestOptions::FORM_PARAMS => [
+                'method' => $function,
+                'parameters' => json_encode($parameters),
+            ],
+        ]);
+        dd($response, $function, $parameters);
         return $this->client()->post('connector.php', [
             RequestOptions::HEADERS => [
                 'X-BLToken' => $this->repository->getIntegrationData()[BaselinkerIntegrationEnum::Token->value]['value'],
